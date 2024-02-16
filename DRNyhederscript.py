d19d4825fc17a2e1f3599064c9a602fdc1c5acec
Alex <alarsen314@outlook.dk>
2024-02-16 16:49:35 +0100

    Adapted code so works for displays notifications for

--- DRNyhederscript.py
+++ DRNyhederscript.py
@@ -4,7 +4,7 @@
 # i) First the title of each news item is scraped using the BeautifulSoup package
 # ii) Then the text associated to each news item is scraped by searching for the parent div section, where the Title occurs. 
 
-##################################################################
+######################### Check permission to scrape #########################################
 import requests
 from urllib.robotparser import RobotFileParser
 
@@ -14,66 +14,63 @@ rp = RobotFileParser()
 rp.set_url(url)
 rp.read()
 can_scrape = rp.can_fetch('*',url)
-print(can_scrape)
 
-##################################################################
+
 if can_scrape:
  # Get request for loading the html code
  response = requests.get(url)
  html = response.text
-
-
- # Parsing the html
- #i) Extracting the title for each news item on the page
+ # Parsing the html using BeautifulSoup package
  from bs4 import BeautifulSoup 
  soup = BeautifulSoup(html,'html.parser')
 
- headings = soup.find_all("div", class_ = "hydra-latest-news-page-short-news-article__top")
- titles=[]
- for heading in headings:
-     title = heading.find("span",class_="dre-title-text")
-     titles.append(title.text)
+#################### Extract header and body for all news types #####################
 
+# Extract all news items 
+ all_news_items = soup.find_all("li", class_ = "hydra-latest-news-page__short-news-item")
+ len(all_news_items)
 
-
- # ii) Filtering on the div section of html code, which corresponds to a specific news item and then extracting the text
-    
  alle_nyheder = []
+ for nyhed in all_news_items:
 
- for title in titles:
+   header_item = nyhed.find("span",class_="dre-title-text").text #Extract header subitem
+   para_items = nyhed.find_all('p') #Extract paragrahh subitems
+   collect = []
+   collect.append(header_item)
+   for item in para_items:
+     p = item.text
+     collect.append(p)
 
-   body = soup.find("article",class_="hydra-latest-news-page-short-news-article",attrs={'aria-label': title})
-   body = body.find_parent('div')
+   alle_nyheder.append(collect)
 
-   body = body.find_all("div", class_ = "dre-speech")
+ len(alle_nyheder)
 
-   bodies=[]
-   for b in body:
-       text = b.text
-       bodies.append(text)
-
-   alle_nyheder.append(bodies)
-
-
+############### Create notifications #######################
 
  from winotify import Notification, audio
  import os 
  cwd = os.getcwd()
  icon_path = os.path.join(cwd, "DRlogo.png")
 
+
  for topic in alle_nyheder:
      title = topic[0]
-     bodies = topic[1:] 
+     bodies = topic[1:]
+  
+     bodies = [str(body) for body in bodies]
+     # Join the body paragraphs into a single string
+     cleaned_string = '\n\n'.join(bodies)
+  
 
-     # Join the paragraphs into a single string
-     body = '\n'.join(bodies)
 
      # Display the toast notification
-     notification = Notification(app_id = " ",title=title, msg=body, icon = icon_path)
+     notification = Notification(app_id = "Kort Nyt",title=title, msg=cleaned_string, icon = icon_path)
     
      notification.add_actions(label="Læs videre her", launch="https://www.dr.dk/nyheder")
      notification.set_audio(audio.Mail, loop=False)
      notification.show()
 
 else:
-   pass 
+    pass 
+
+
